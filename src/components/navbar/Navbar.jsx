61086623b0addc805da06ca1b8411623328076cc
Raamdeluxe <Robin@raamdeluxe.nl>
2023-12-04 23:25:41 +0100

    add linkedIn profile link

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -5,7 +5,13 @@ export default function Navbar() {
   return (
     <nav className={styles.nav__container}>
       <div className={styles.icon__container}>
-        <img src={LinkedIn} alt="LinkedIn" />
+        <a
+          href="https://www.linkedin.com/in/robinkonijnendijk/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <img src={LinkedIn} alt="LinkedIn" />
+        </a>
       </div>
       <ul className={styles.nav__links}>
         <li className={styles.nav__links__item}>Tech Stack</li>
